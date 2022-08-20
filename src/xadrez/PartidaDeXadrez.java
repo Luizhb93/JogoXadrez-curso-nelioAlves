a028725e9f0a507bb0864003414c9d11b7710535
Luiz Brito <luisshb93@gmail.com>
2022-08-20 18:17:24 -0300

    Implementando exceções no xadrez.

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -23,9 +22,14 @@ public class PartidaDeXadrez {
 		}
 		return matriz;
 	}
+	
+	private void colocarNovaPeca(char coluna, int linha, PecasDeXadrez peca) {
+		tabuleiro.colocarPeca(peca, new XadrezPosicao(coluna, linha).paraPosicao());
+	}
+	
 	private void iniciarPartida() {
-		tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.BRANCO), new Posicao(2,1));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.PRETO ), new Posicao(0,4));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BRANCO ), new Posicao(7,4));
+		colocarNovaPeca('b',6,new Torre(tabuleiro,Cor.BRANCO));
+		colocarNovaPeca('e',8,new Rei(tabuleiro, Cor.PRETO ));
+		colocarNovaPeca('e',1,new Rei(tabuleiro, Cor.BRANCO ));
 	}
 }
